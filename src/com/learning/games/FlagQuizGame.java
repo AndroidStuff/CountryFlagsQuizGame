81f28e263bd2b534d9d611dcdf6c9717b20b1992
Karthik Sirasanagandla <sirkarthik@gmail.com>
2016-01-12 20:15:11 +0530

    Initial commit of Prototype

---
+++ src/com/learning/games/FlagQuizGame.java
@@ -0,0 +1,15 @@
+package com.learning.games;
+
+import com.example.flagquizgame.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FlagQuizGame extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_flag_quiz_game);
+	}
+}
